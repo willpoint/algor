4ff61cadfc23be91056b96b1af4d88939e0ffde0
Uzondu Enudeme <willpoint@gmail.com>
2018-08-04 18:15:11 +0100

    provide comment on insertion sort and it's helper function

--- pkg/sorting/insertion.go
+++ pkg/sorting/insertion.go
@@ -1,6 +1,18 @@
 package sorting
 
-// InsertionSort ...
+// InsertionSort is achieved by spliting the
+// given collection into two subcollections
+// an already sorted collection and a yet to be
+// sorted collection
+// When the invariant is maintained that every
+// new insertion into the sorted subcollection leaves
+// it still sorted, the entire collection is then sorted after all
+// elements from the unsorted subcollection are inserted
+// into the sorted subcollection
+// This algorithm works best for a small number of elements
+// or when the elements it contain exist in a nearly sorted form
+// In time complexity - it's best case is 0(n)
+// and it's average and worst case is 0(n²)
 func InsertionSort(b []byte) []byte {
 	for i := 1; i < len(b); i++ {
 		insert(b, i, b[i])
@@ -8,6 +20,9 @@ func InsertionSort(b []byte) []byte {
 	return b
 }
 
+// insert inserts the element at the given position `pos`
+// in the right position that keeps the sorted subcollection
+// sorted, therefore maintaning the invariant
 func insert(b []byte, pos int, val byte) {
 	i := pos - 1
 	for i >= 0 && b[i] > val {
