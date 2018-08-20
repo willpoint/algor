2cd26592f548ecec38b3f3e2219f55eee4980d4c
Uzondu Enudeme <willpoint@gmail.com>
2018-08-20 13:20:28 +0100

    add redbacktree to package ds

--- pkg/ds/bst.go
+++ pkg/ds/bst.go
@@ -2,37 +2,41 @@ package ds
 
 // BNode is a binary search tree node
 type BNode struct {
-	E     string
-	Left  *BNode
-	Right *BNode
+	Key    int
+	Parent *BNode
+	Left   *BNode
+	Right  *BNode
 }
 
 // BST is a binary search tree with root
+// A BST has an invariant - that for all nodes x:
+// if y is in the left subtree of x
+// the Key(y) <= Key(x)
+// and if y is in the right subtree of x
+// then Key(y) >= Key(x)
 type BST struct {
 	Root *BNode
 }
 
 // NewBST ...
-func NewBST(e string) *BST {
-	root := &BNode{
-		E: e,
-	}
+func NewBST() *BST {
+	root := &BNode{}
 	return &BST{root}
 }
 
 // Insert ...
-func (t *BST) Insert(e string) {
+func (t *BST) Insert(e int) {
 
 	var parent *BNode
 
 	nn := &BNode{
-		E: e,
+		Key: e,
 	}
 
 	currNode := t.Root
 	for currNode != nil {
 		parent = currNode
-		if currNode.E < nn.E {
+		if currNode.Key < nn.Key {
 			parent = currNode.Left
 		} else {
 			parent = currNode.Right
@@ -41,9 +45,39 @@ func (t *BST) Insert(e string) {
 
 	if parent == nil {
 		t.Root = nn
-	} else if parent.E < nn.E {
+	} else if parent.Key < nn.Key {
+		nn.Parent = parent
 		parent.Left = nn
 	} else {
+		nn.Parent = parent
 		parent.Right = nn
 	}
+}
+
+// Min returns the min key
+func (t *BST) Min() int {
+	curr := t.Root
+
+	if curr.Left == nil {
+		return 0
+	}
+
+	for curr.Left != nil {
+		curr = curr.Left
+	}
+	return curr.Key
+}
+
+// Max returns the max key
+func (t *BST) Max() int {
+	curr := t.Root
+
+	if curr.Right == nil {
+		return 0
+	}
+
+	for curr.Right != nil {
+		curr = curr.Right
+	}
+	return curr.Key
 }
