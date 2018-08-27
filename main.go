778f13174e478b74d656d4643da1f6b340ff281d
Uzondu Enudeme <uzondu.enudeme@orbsolutions.com.ng>
2018-08-27 23:53:55 +0100

    add test for matrix's methods, and methods to calculate matrix multiplication, transpose, identity

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/willpoint/algor/pkg/ds"
+)
+
 func main() {
+
+	m := ds.NewMatrix(2, 3)
+	n := ds.NewMatrix(3, 2)
+
+	_ = m.SetRowSlice(0, []float64{-1, 2, 0})
+	_ = m.SetRowSlice(1, []float64{4, 1, -2})
+
+	_ = n.SetRowSlice(0, []float64{1, 3})
+	_ = n.SetRowSlice(1, []float64{0, -1})
+	_ = n.SetRowSlice(2, []float64{7, 5})
+
+	o, _ := m.Multiply(n)
+	j := o.Transpose()
+	fmt.Printf("%#v\n", j.Array())
+
+	t := ds.NewMatrix(2, 3)
+	_ = t.SetRowSlice(0, []float64{2, 4, -1})
+	_ = t.SetRowSlice(1, []float64{0, 3, 5})
+	t1 := t.Transpose()
+	fmt.Printf("%#v\n", t1.Array())
+
+	z := ds.Identity(3)
+	fmt.Printf("%#v\n", z.Array())
+
+	q := ds.NewMatrix(3, 3)
+	_ = q.SetRowSlice(0, []float64{-1, 2, 0})
+	_ = q.SetRowSlice(1, []float64{4, 1, -2})
+	_ = q.SetRowSlice(2, []float64{8, 6, -1})
+
+	r, _ := q.Multiply(z)
+	fmt.Printf("%#v\n", r.Array())
 
 }
--- pkg/ds/matrix.go
+++ pkg/ds/matrix.go
@@ -10,6 +10,16 @@ var (
 	// ErrColumnNotEqualRow is a multiplication error that is supported only for matrices
 	// where the column on the lhs is equal to column on the rhs
 	ErrColumnNotEqualRow = errors.New("column of matrix i is not equal to row of matrix j")
+	//ErrUnmatchedRow occurs when the length of the slice provided to set a row slice
+	// does not match the length of the cols of the matrix to mutate
+	ErrUnmatchedRow = errors.New("row provided does not match matrix cols")
+	//ErrUnmatchedCol occurs when the length of the slice provided to set a row slice
+	// does not match the length of the cols of the matrix to mutate
+	ErrUnmatchedCol = errors.New("col provided does not match matrix rows")
+	// ErrRowOutOfBound ...
+	ErrRowOutOfBound = errors.New("row out out bound")
+	// ErrColOutOfBound ...
+	ErrColOutOfBound = errors.New("col out out bound")
 )
 
 // Matrix is backed by a flat slice of elements
@@ -24,7 +34,7 @@ type Matrix struct {
 	step int
 }
 
-// NewMatrix create a new matrix
+// NewMatrix creates and returns a pionter to a new matrix
 func NewMatrix(rows, cols int) *Matrix {
 	return &Matrix{
 		rows:     rows,
@@ -63,8 +73,13 @@ func (m *Matrix) Arrays() [][]float64 {
 	return n
 }
 
-// ColumnSlice returns a single column of the matrix grid
-func (m *Matrix) ColumnSlice(j int) []float64 {
+// Array returns the compressed array representing the matrix m
+func (m *Matrix) Array() []float64 {
+	return m.elements
+}
+
+// ColSlice returns a single column of the matrix grid
+func (m *Matrix) ColSlice(j int) []float64 {
 	c := make([]float64, m.rows)
 	for i := 0; i < m.rows; i++ {
 		c[i] = m.elements[i*m.step+j]
@@ -74,7 +89,38 @@ func (m *Matrix) ColumnSlice(j int) []float64 {
 
 // RowSlice returns a single row of the matrix grid
 func (m *Matrix) RowSlice(i int) []float64 {
-	return m.elements[i*m.step : i*m.step+m.cols]
+	c := make([]float64, m.cols)
+	copy(c, m.elements[i*m.step:i*m.step+m.cols])
+	return c
+}
+
+// SetRowSlice sets a single row of the matrix with the given slice
+func (m *Matrix) SetRowSlice(row int, s []float64) error {
+	if len(s) != m.cols {
+		return ErrUnmatchedRow
+	}
+	if row >= m.rows || row < 0 {
+		return ErrRowOutOfBound
+	}
+	for i := 0; i < m.cols; i++ {
+		m.elements[row*m.step+i] = s[i]
+	}
+	return nil
+}
+
+// SetColSlice set a single col of the matrix m to the elements
+// of the given slice s
+func (m *Matrix) SetColSlice(col int, s []float64) error {
+	if len(s) != m.rows {
+		return ErrUnmatchedCol
+	}
+	if col >= m.cols || col < 0 {
+		return ErrColOutOfBound
+	}
+	for i := 0; i < m.rows; i++ {
+		m.elements[i*m.step+col] = s[i]
+	}
+	return nil
 }
 
 // Get returns the element in the ith row and jth column
@@ -84,7 +130,7 @@ func (m *Matrix) Get(i, j int) float64 {
 
 // Set sets the scalar element val in the ith row and jth col
 func (m *Matrix) Set(i, j int, val float64) {
-	m.elements[i*m.step : i*m.step+j][j] = val
+	m.elements[i*m.step+j] = val
 }
 
 // ScaleBy multiplies each element of the matrix by the given scalar value
@@ -95,7 +141,7 @@ func (m *Matrix) ScaleBy(s float64) {
 }
 
 // Add creates and returns a new matrix that is the result of adding
-// this matrix to the given matrix, the two matrices must be of same dimension
+// matrix m to n, the two matrices must have equal dimensions
 func (m *Matrix) Add(n *Matrix) (*Matrix, error) {
 	if m.rows != n.rows || m.cols != n.cols {
 		return nil, ErrDifferentMatrixDimension
@@ -119,3 +165,51 @@ func (m *Matrix) Subtract(n *Matrix) (*Matrix, error) {
 	}
 	return o, nil
 }
+
+// Multiply creates and returns a matrix whose result is the multiplication
+// of matrices m and n. The cols of m must be equal to the rows n
+func (m *Matrix) Multiply(n *Matrix) (*Matrix, error) {
+	if m.cols != n.rows {
+		return nil, ErrColumnNotEqualRow
+	}
+
+	o := NewMatrix(m.rows, n.cols)
+
+	for i := 0; i < m.rows; i++ {
+		for j := 0; j < n.cols; j++ {
+			sum := float64(0)
+			for k := 0; k < m.cols; k++ {
+				sum += m.elements[i*m.step+k] * n.Get(k, j)
+			}
+			o.elements[i*o.step+j] = sum
+		}
+	}
+	return o, nil
+}
+
+// Transpose ...
+func (m *Matrix) Transpose() *Matrix {
+	o := NewMatrix(m.cols, m.rows)
+	for i := 0; i < o.rows; i++ {
+		for j := 0; j < o.cols; j++ {
+			o.elements[i*o.step+j] = m.Get(j, i)
+		}
+	}
+	return o
+}
+
+// Identity ...
+func Identity(s int) *Matrix {
+	o := NewMatrix(s, s)
+	for i := 0; i < o.rows; i++ {
+		o.elements[i*o.step+i] = 1
+	}
+	return o
+}
+
+// Determinant of A, denoted by |A| for a given 2x2 matrix
+// (a b)
+// (c d)
+// is given as ab - bc
+// however for larger  matrices `minors` and `cofactors` are
+// required
